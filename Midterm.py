a28b861a09195cd716f5064c006e73321049dda2
zhuethan-871 <zhuethan@gmail.com>
2026-01-13 10:51:00 -0500

    Update Midterm.py

--- Midterm.py
+++ Midterm.py
@@ -2,34 +2,66 @@ import math
 
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
-    return 0.0
+    area_of_circle = math.pi * radius ** 2
+
+    return round(area_of_circle, 2)
 
 # Q2: Hollow Right Triangle
 def hollow_right_triangle(n):
-    return ""
+    output = ""
+    
+    if n < 4:
+        output = "The triangle height should be at least 4."
+    else:
+        for i in range(n):
+            for j in range(i + 1):
+                if i == n - 1 or j == 0:
+                    output += "*"
+                elif i == j:
+                    output += "*"
+                else:
+                    output += " "
+
+            output += "\n"
+
+    return output.rstrip()
 
 # Q3: Inverted Pyramid
 def inverted_pyramid(n):
-    return ""
+    output = ""
+
+    if n < 3:
+        output = "The pyramid height should be at least 3."
+    else:
+        for i in range(n):
+            for j in range(i):
+                output += " "
+
+            for k in range(2 * (n - i ) - 1):
+                output += "*"
+
+            output += "\n"
+
+    return output.rstrip()
 
 # ----------------------------------------------------------------
-print(area_of_circle(5))
-print()
+# print(area_of_circle(5))
+# print()
 
-print(hollow_right_triangle(3))
-print()
+# print(hollow_right_triangle(3))
+# print()
 
-print(hollow_right_triangle(4))
-print()
+# print(hollow_right_triangle(4))
+# print()
 
-print(hollow_right_triangle(5))
-print()
+# print(hollow_right_triangle(5))
+# print()
 
-print(inverted_pyramid(3))
-print()
+# print(inverted_pyramid(3))
+# print()
 
-print(inverted_pyramid(4))
-print()
+# print(inverted_pyramid(4))
+# print()
 
-print(inverted_pyramid(5))
-print()
+# print(inverted_pyramid(5))
+# print()
